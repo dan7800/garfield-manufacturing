c034482568fa2667eee9664be446f4a9dd7949fc
Brian Besmanoff <bbesmanoff@gmail.com>
2016-04-13 00:41:06 -0400

    Remove sample

--- src/main/java/edu/rit/se/swen343/GarfieldManufacturing.java
+++ src/main/java/edu/rit/se/swen343/GarfieldManufacturing.java
@@ -2,7 +2,6 @@ package edu.rit.se.swen343;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
-import edu.rit.se.swen343.resources.OrderResource;
 
 /**
  * Basic configuration for the application.
@@ -36,6 +35,5 @@ public class GarfieldManufacturing extends
 
         env.healthChecks().register("application",
                 new GarfieldManufacturingHealthCheck());
-        env.jersey().register(new OrderResource());
     }
 }
